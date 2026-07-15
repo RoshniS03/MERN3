a35962dd348d6fb4484417e6b448fb35bef9a53f
RoshniS03 <roshshahumvlu@gmail.com>
2026-07-15 13:38:23 +0530

    Create 3b1.jsx

---
+++ 3b1.jsx
@@ -0,0 +1,17 @@
+function App ()
+{
+  const emp = "Varun";
+  const dept = "IT";
+  const sal = 50000;
+  return (
+    <div>
+      <h1>Emplyoee Details</h1>
+      <p>Name: {emp}</p>
+      <p>Department: {dept}</p>
+      <p>Salary: {sal}</p>
+      <p>Roshni T041</p>
+     
+    </div>
+  );
+}
+export default App
